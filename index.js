b343eca689c86fed03f13f8d51bb96ae64198276
jose barboza <jrbarboza08@gmail.com>
2018-06-01 17:37:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,53 @@
+function theBeatlesPlay(musicans,instruments){
+  
+  var array = [];
+  
+  for (let i = 0; i < 4; i++) {
+  
+  array.push(musicans[i] + ' plays ' + instruments[i])
+  
+}
+  
+  return array
+  
+  
+}
+
+function johnLennonFacts (facts){
+  
+  var array = [];
+  
+  let i = 0;
+  
+while (i < facts.length) {
+  
+  
+ array.push(facts[i] + '!!!');
+  
+i++;
+}
+  return array;
+  
+}
+
+
+var ILoveTheBeatles = function(n) {
+
+
+
+
+var array = [];
+  
+do{
+    
+    array.shift("I Love The Beatles!");
+    
+    n++;
+    
+  }while (n < 15);
+  
+
+
+  return array;
+  
+}
